abd553fdadcf484894d3a32d63dc91f0666de27d
Nova <160991172+deeznustjnc@users.noreply.github.com>
2026-03-09 14:59:38 +0000

    ready for railway deploy

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import { createServer } from "node:http";
+import { fileURLToPath } from "url";
+import { hostname } from "node:os";
+import { server as wisp, logging } from "@mercuryworkshop/wisp-js/server";
+import Fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+import { scramjetPath } from "@mercuryworkshop/scramjet/path";
+import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
+import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
+
+const publicPath = fileURLToPath(new URL("../public/", import.meta.url));
+
+logging.set_level(logging.NONE);
+Object.assign(wisp.options, {
+  allow_udp_streams: true,
+  dns_servers: ["8.8.8.8", "8.8.4.4", "1.1.1.1"],
+  hostname_blacklist: [],
+});
+
+const fastify = Fastify({
+  serverFactory: (handler) => {
+    return createServer()
+      .on("request", (req, res) => {
+        res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+        res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+        res.setHeader("Cache-Control", "public, max-age=3600");
+        handler(req, res);
+      })
+      .on("upgrade", (req, socket, head) => {
+        if (req.url.endsWith("/wisp/")) wisp.routeRequest(req, socket, head);
+        else socket.end();
+      });
+  },
+});
+
+fastify.register(fastifyStatic, { root: publicPath, decorateReply: true });
+fastify.register(fastifyStatic, { root: scramjetPath, prefix: "/scram/", decorateReply: false });
+fastify.register(fastifyStatic, { root: libcurlPath, prefix: "/libcurl/", decorateReply: false });
+fastify.register(fastifyStatic, { root: baremuxPath, prefix: "/baremux/", decorateReply: false });
+
+fastify.setNotFoundHandler((res, reply) => {
+  return reply.code(404).type("text/html").sendFile("404.html");
+});
+
+fastify.server.on("listening", () => {
+  const address = fastify.server.address();
+  console.log("Listening on:");
+  console.log(`\thttp://localhost:${address.port}`);
+});
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+function shutdown() {
+  fastify.close();
+  process.exit(0);
+}
+
+let port = parseInt(process.env.PORT || "");
+if (isNaN(port)) port = 8080;
+fastify.listen({ port, host: "0.0.0.0" });
